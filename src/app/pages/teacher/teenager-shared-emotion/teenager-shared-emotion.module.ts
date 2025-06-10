8293257cce51ac3a3bee884cab5621ed73f7608b
VietPB <viet220994@gmail.com>
2025-06-11 01:25:23 +0700

    feat(teacher-emotion-shared): UI/UX cho trang chia sẻ cảm xúc của học sinh

--- src/app/pages/teacher/teenager-shared-emotion/teenager-shared-emotion.module.ts
+++ src/app/pages/teacher/teenager-shared-emotion/teenager-shared-emotion.module.ts
@@ -8,15 +8,17 @@ import { TeenagerSharedEmotionPageRoutingModule } from './teenager-shared-emotio
 
 import { TeenagerSharedEmotionPage } from './teenager-shared-emotion.page';
 import { SharedModule } from '../../../shared/shared.module';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        FormsModule,
-        IonicModule,
-        TeenagerSharedEmotionPageRoutingModule,
-        SharedModule
-    ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    TeenagerSharedEmotionPageRoutingModule,
+    SharedModule,
+    TranslatePipe
+  ],
   declarations: [TeenagerSharedEmotionPage]
 })
 export class TeenagerSharedEmotionPageModule {}
